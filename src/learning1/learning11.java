78262da505f1da0868a1c4241587749ffd3293ea
acer <acer@10.0.0.16>
2018-01-23 23:18:45 -0800

    first commit

---
+++ src/learning1/learning11.java
@@ -0,0 +1,12 @@
+package learning1;
+
+public class learning11 {
+
+	public static void main(String[] args) {
+		System.out.println("hellow world");
+		System.out.println("shut up !! we are trying ti sleep!!!!");
+		System.out.println("hhhhhhh not funny !!");
+
+	}
+
+}
